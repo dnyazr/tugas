2cec7897de3b0217090b0142df47c8fdd8291a37
dnyazr <85937643+dnyazr@users.noreply.github.com>
2021-06-15 20:36:36 +0800

    meghapus id container

--- welcome_message.php
+++ welcome_message.php
@@ -58,10 +58,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		margin: 20px 0 0 0;
 	}
 
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
+
 	}
 	</style>
 </head>
