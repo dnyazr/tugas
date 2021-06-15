97a136cbe2040b384be7753b4b2dc79cbf31b435
dnyazr <85937643+dnyazr@users.noreply.github.com>
2021-06-15 20:34:12 +0800

    merubah button buat dan hapus password
    merubah button buat menjadi btn-primary dan menghapus form-group password

--- create.php
+++ create.php
@@ -34,13 +34,10 @@
 				<input type="text" name="username" value="<?php echo set_value('username'); ?>" class="form-control">
 				<?php echo form_error('username'); ?>
 			</div>
-			<div class="form-group">
-				<label>Password</label>
-				<input type="text" name="password" value="<?php echo set_value('password'); ?>" class="form-control">
-				<?php echo form_error('password'); ?>
+			
 			</div>
 			<div class="form-group">
-				<button class="btn btn-danger">BUAT</button>
+				<button class="btn btn-primary">BUAT</button>
 				<a href="<?php echo base_url().'index.php/user/index'; ?>" class="btn btn-secondary">BATAL</a>
 			</div>
 		</div>
