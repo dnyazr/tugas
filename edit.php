13b3daa025a97ba32a5d4d1a3f656f86dd52d2a5
dnyazr <85937643+dnyazr@users.noreply.github.com>
2021-06-15 20:35:16 +0800

    menghapus button edit

--- edit.php
+++ edit.php
@@ -34,10 +34,7 @@
 				<label>Password</label>
 				<input type="text" name="password" value="<?php echo set_value('password',$user['password']); ?>" class="form-control">
 				<?php echo form_error('password'); ?>
-			</div>
-			<div class="form-group">
-				<button class="btn btn-primary">EDIT</button>
-				<a href="<?php echo base_url().'index.php/user/index'; ?>" class="btn btn-secondary">BATAL</a>
+		
 			</div>
 		</div>
 	</div>
